f156e7dcae1725e2f9ddbc1ef7abceffb76ad75a
Glurth <Glurth@gmail.com>
2025-07-04 09:00:21 -0600

    minor optimization to poly draw maze setup of matrixes

--- Runtime/Templates/PolyhedronSurface/PolyhedronMazeDraw.cs
+++ Runtime/Templates/PolyhedronSurface/PolyhedronMazeDraw.cs
@@ -22,7 +22,73 @@ namespace Eye.Maps.Templates
         {
             return new FaceCoordinate(facesAndNeighbors,facesAndNeighbors.faceDetails.Count);// facesAndNeighbors.faceDetails[facesAndNeighbors.faceDetails.Count - 1].neighborIndices, facesAndNeighbors.faceDetails[facesAndNeighbors.faceDetails.Count - 1].normal);
         }
+
+        int[] meshIndexToUniqueID;
+        Mesh lastMesh = null;
+        void PopulateMeshIndexToUniqueID()
+        {
+            if (lastMesh == facesAndNeighbors.meshRef) return;
+            Mesh mesh = facesAndNeighbors.meshRef;
+            lastMesh = mesh;
+            Dictionary<Vector3, int> uniqueVerts = new Dictionary<Vector3, int>();
+            meshIndexToUniqueID = new int[mesh.vertexCount];
+            int uniqueIndexCounter = 0;
+
+            for (int i = 0; i < mesh.vertexCount; i++)
+            {
+                Vector3 v = mesh.vertices[i];
+                if (!uniqueVerts.TryGetValue(v, out int uniqueIdx))
+                {
+                    uniqueIdx = uniqueIndexCounter++;
+                    uniqueVerts[v] = uniqueIdx;
+                }
+                meshIndexToUniqueID[i] = uniqueIdx;
+            }
+        }
         protected override Matrix4x4 GetNeighborWallMatrix(FaceCoordinate coord, int neighborIndex, Vector3 tilePosition, int neighborCount)
+        {
+            //we will use facesAndNeighbors to get ref to mesh, and vertex index of two coners these neighbors touch. (note the faces will NOT share the same vertex index, but they will have the same 3d value [cuz normals per face])
+            //then using these as the endpoints, we will generate a maxtrix for a box, scaled to be thin along the line between them, and normal/flush with the face.
+            //the cube will be drawn on both sides/faces and should be offset such that they will exactly touch at the apex of where they meet (and overlap beneath)
+            // float wallThicknessFraction, wallHeightFraction will define how thick/deep and tall the line should be, as a fraction of it's length
+            PopulateMeshIndexToUniqueID();//returns if done already
+            Mesh mesh = facesAndNeighbors.meshRef;
+            FaceCoordinate neighbor = coord.GetNeighbor(neighborIndex);
+            FaceDetails coordFaceDetails = facesAndNeighbors.faceDetails[coord.faceIndex];
+            FaceDetails neighborFaceDetails = facesAndNeighbors.faceDetails[neighbor.faceIndex];
+            List<Vector3> endpoints = new List<Vector3>(2);
+            foreach (int faceCornerVertIndex in coordFaceDetails.cornerVertexMeshIndices)
+            {   
+                int cornerUniqueID = meshIndexToUniqueID[faceCornerVertIndex];
+                foreach (int neighborCornerVertIndex in neighborFaceDetails.cornerVertexMeshIndices)
+                {
+                    int neighborCornerUniqueID= meshIndexToUniqueID[neighborCornerVertIndex];
+                   // Vector3 posN = mesh.vertices[neighborCornerVertIndex];
+                    if (cornerUniqueID == neighborCornerUniqueID)
+                    {
+                        //match
+                        endpoints.Add(mesh.vertices[faceCornerVertIndex]);
+                        break;
+                    }
+                }
+                if (endpoints.Count > 1) break;
+            }
+            if (endpoints.Count < 2) throw new System.Exception("Unexpected processing- unable to find matching corners for faces: [" + coord + "] ,[" + neighbor + "]");
+            Vector3 edge = (endpoints[0] - endpoints[1]);
+            Vector3 edgeDir = edge.normalized;
+            Vector3 edgeCenterPos = (endpoints[0] + endpoints[1]) / 2f;
+            Vector3 edgeNormal = (coordFaceDetails.normal + neighborFaceDetails.normal) * 0.5f;
+            if (Mathf.Abs(Vector3.Dot(edgeNormal, edgeDir)) > 0.03f)
+                Debug.LogWarning("Error: edge and edge normal are not perpendicular");
+            // Debug.Log("Edge Normal:" + edgeNormal + "  Edge dir:" + edgeDir  + "Edge:" + edge);
+
+            Quaternion wallRot = Quaternion.LookRotation(edgeDir, edgeNormal); //rotates forward (z-axis) to look down length of edge, with up pointing directly away from face.
+            float edgeLen = edge.magnitude;
+            Vector3 wallScale = new Vector3(wallThicknessFraction, wallHeightFraction, 1) * edgeLen;
+            return Matrix4x4.TRS(edgeCenterPos, wallRot, wallScale);
+
+        }
+        protected Matrix4x4 OLDGetNeighborWallMatrix(FaceCoordinate coord, int neighborIndex, Vector3 tilePosition, int neighborCount)
         {
             //we will use facesAndNeighbors to get ref to mesh, and vertex index of two coners these neighbors touch. (note the faces will NOT share the same vertex index, but they will have the same 3d value [cuz normals per face])
             //then using these as the endpoints, we will generate a maxtrix for a box, scaled to be thin along the line between them, and normal/flush with the face.
