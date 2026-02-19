0def0d0a777108f2017a772b265840e9d29728d5
Glurth <Glurth@gmail.com>
2026-02-19 13:26:56 -0700

    fixed getpathneighbor- fixed wrong var ref

--- Runtime/ITileCoordinate.cs
+++ Runtime/ITileCoordinate.cs
@@ -140,9 +140,9 @@ namespace EyE.Maps
         {
             T spatialNeighbor = coord.GetSpatialNeighbor(neighborIndex);
             if (teleporters == null) return spatialNeighbor;
-           // if (teleporters.TryGetValue(spatialNeighbor, out T teleportDestination))//is coord a teleporter source?
-             //   return teleportDestination.GetPathNeighbor(neighborIndex, teleporters);
-            if (teleporters.TryGetValue(spatialNeighbor, out T teleportDestination))
+            if (teleporters.TryGetValue((T)coord, out T teleportDestination))//is coord a teleporter source?
+                return teleportDestination.GetPathNeighbor(neighborIndex, teleporters);
+            if (teleporters.TryGetValue(spatialNeighbor, out teleportDestination))
                 return teleportDestination;
             return spatialNeighbor;
         }
